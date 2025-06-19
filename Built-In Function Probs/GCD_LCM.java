5d027abe70bec05ff08617f87e062ca84dab3b2b
Vipransh Upadhyay <codvi001@gmail.com>
2025-06-19 11:50:54 +0530

    [Vipransh Upadhyay] Add GCD_LCM

---
+++ Built-In Function Probs/GCD_LCM.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class GCD_LCM {
+
+    public static int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
+    }
+
+    public static int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
+    }
+
+    public static void displayResults(int a, int b, int gcd, int lcm) {
+        System.out.println("GCD of " + a + " and " + b + " is " + gcd);
+        System.out.println("LCM of " + a + " and " + b + " is " + lcm);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter two numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int gcdResult = gcd(a, b);
+        int lcmResult = lcm(a, b);
+        displayResults(a, b, gcdResult, lcmResult);
+    }
+}
