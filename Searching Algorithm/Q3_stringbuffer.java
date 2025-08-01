de799f3571ae36fb900e72b1ddec96e184802173
Vipransh Upadhyay <115969214+codvi@users.noreply.github.com>
2025-08-01 09:06:53 +0530

    [Vipransh Upadhyay] add Q3_stringbuffer

---
+++ Searching Algorithm/Q3_stringbuffer.java
@@ -0,0 +1,14 @@
+
+public class Q3_stringbuffer {
+    public static String concatStrings(String[] arr) {
+        StringBuffer sb = new StringBuffer();
+        for (String s : arr) {
+            sb.append(s);
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        String[] arr = {"Hello", " ", "World", "!"};
+        System.out.println("Concatenated: " + concatStrings(arr));
+    }
+}
