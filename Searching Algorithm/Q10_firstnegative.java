0de82611b90fb35391e1a721fc77a58f50759e10
Vipransh Upadhyay <115969214+codvi@users.noreply.github.com>
2025-08-01 09:11:15 +0530

    [Vipransh Upadhyay] add Q10_firstnegative

---
+++ Searching Algorithm/Q10_firstnegative.java
@@ -0,0 +1,12 @@
+public class Q10_firstnegative {
+    public static int findFirstNegative(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 0) return i;
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 7, -2, 8};
+        System.out.println("First negative index: " + findFirstNegative(arr));
+    }
+}
