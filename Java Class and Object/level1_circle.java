1c17287b60ed04fa3dd5ceebbdbbe4baddf25ed3
Vipransh Upadhyay <115969214+codvi@users.noreply.github.com>
2025-08-01 11:08:19 +0530

    [Vipransh Upadhyay] add level1_circle

---
+++ Java Class and Object/level1_circle.java
@@ -0,0 +1,26 @@
+public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double calculateCircumference() {
+        return 2 * Math.PI * radius;
+    }
+
+    public void displayDetails() {
+        System.out.println("Radius: " + radius);
+        System.out.println("Area: " + String.format("%.2f", calculateArea()));
+        System.out.println("Circumference: " + String.format("%.2f", calculateCircumference()));
+    }
+
+    public static void main(String[] args) {
+        Circle circle = new Circle(5.0);
+        circle.displayDetails();
+    }
+}
